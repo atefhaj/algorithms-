fe952717f7d747f3a3a82d0cde800940ca12049d
atefhaj <53941981+atefhaj@users.noreply.github.com>
2023-11-24 10:51:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
-perfecthalf = ((1 / 37) * 360) / 2;
+document.getElementById('signup-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+  // Get input values
+  var username = document.getElementById('username').value.trim();
+  var email = document.getElementById('email').value.trim();
+  var password = document.getElementById('password').value.trim();
+  
+  // Simple email validation
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    alert('البريد الإلكتروني غير صالح');
+    return;
+  }
+  
+  // Simple password length check
+  if (password.length < 6) {
+    alert('يجب أن تكون كلمة المرور على الأقل 6 أحرف');
+    return;
+  }
 
-let currentLength = perfecthalf;
+  // إضافة المزيد من الكود هنا للتعامل مع بيانات الاشتراك بشكل أوسع
+  // يمكن استخدام طرق أكثر أماناً لتخزين كلمات المرور والتحقق من البريد الإلكتروني
+  
+  // إرسال البيانات للسيرفر أو معالجتها بشكل أخر
 
-$(".wheel img").css("transform", "rotate(" + perfecthalf + "deg)");
-
-$(".spin").click(() => {
-  $(".wheel img").css("filter", "blur(8px)");
-
-  spininterval = getRandomInt(0, 37) * (360 / 37) + getRandomInt(3, 4) * 360;
-  currentLength += spininterval;
-
-  numofsecs = spininterval;
-
-  console.log(currentLength);
-  $(".wheel img").css("transform", "rotate(" + currentLength + "deg)");
-
-  setTimeout(function () {
-    $(".wheel img").css("filter", "blur(0px)");
-  }, numofsecs);
+  // إعادة توجيه المستخدم إلى صفحة تسجيل الدخول أو أي صفحة أخرى بناءً على الحاجة
 });
-
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-$(document).ready(function() {
-  $(".spin").click();
-})
