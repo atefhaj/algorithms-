2decd92e696f59b5039a889f65eb6f4d2e18bf87
atefhaj <53941981+atefhaj@users.noreply.github.com>
2023-11-25 10:08:21 +0100

    Update YourModelLoader.js

--- YourModelLoader.js
+++ YourModelLoader.js
@@ -1,4 +1,19 @@
-// ...
+const scene = new THREE.Scene();
+
+// إنشاء الكاميرا
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+// إنشاء المشهد (مكعب)
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+// إنشاء المُظهر (renderer)
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 // تحديث موضع المكعب بواسطة اللمس
 let isDragging = false;
