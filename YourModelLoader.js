51b277893b1869c5b62280e9d9566e335083dce2
atefhaj <53941981+atefhaj@users.noreply.github.com>
2023-11-25 10:12:25 +0100

    Update YourModelLoader.js

--- YourModelLoader.js
+++ YourModelLoader.js
@@ -16,22 +16,32 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 // تحديث موضع المكعب بواسطة اللمس
-let isDragging = false;
+ اللمس
 let previousTouchX = 0;
+let previousTouchY = 0;
+let isDragging = false;
 
 // تفاعل مع بداية اللمس
 renderer.domElement.addEventListener('touchstart', function (event) {
   isDragging = true;
   previousTouchX = event.touches[0].clientX;
+  previousTouchY = event.touches[0].clientY;
 }, false);
 
 // تفاعل مع حركة اللمس
 renderer.domElement.addEventListener('touchmove', function (event) {
   if (isDragging) {
     const touchX = event.touches[0].clientX;
+    const touchY = event.touches[0].clientY;
+
     const deltaX = touchX - previousTouchX;
-    cube.rotation.y += deltaX * 0.01; // تحريك المكعب باللمس
+    const deltaY = touchY - previousTouchY;
+
+    cube.rotation.y += deltaX * 0.01; // تحريك المكعب يمينًا ويسارًا
+    cube.rotation.x += deltaY * 0.01; // تحريك المكعب أعلى وأسفل
+
     previousTouchX = touchX;
+    previousTouchY = touchY;
   }
 }, false);
 
@@ -39,10 +49,3 @@ renderer.domElement.addEventListener('touchmove', function (event) {
 renderer.domElement.addEventListener('touchend', function (event) {
   isDragging = false;
 }, false);
-
-// دورة التحديث للرسم
-function animate() {
-  requestAnimationFrame(animate);
-  renderer.render(scene, camera);
-}
-animate();
