5f683e6c2def5b513a857ef4d28d4b138d3cb302
atefhaj <53941981+atefhaj@users.noreply.github.com>
2023-11-25 10:06:15 +0100

    Update YourModelLoader.js

--- YourModelLoader.js
+++ YourModelLoader.js
@@ -1,25 +1,33 @@
-const scene = new THREE.Scene();
+// ...
 
-// إنشاء الكاميرا
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
+// تحديث موضع المكعب بواسطة اللمس
+let isDragging = false;
+let previousTouchX = 0;
 
-// إنشاء المشهد (هنا مثال لمكعب)
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+// تفاعل مع بداية اللمس
+renderer.domElement.addEventListener('touchstart', function (event) {
+  isDragging = true;
+  previousTouchX = event.touches[0].clientX;
+}, false);
 
-// إنشاء المُظهر (renderer)
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+// تفاعل مع حركة اللمس
+renderer.domElement.addEventListener('touchmove', function (event) {
+  if (isDragging) {
+    const touchX = event.touches[0].clientX;
+    const deltaX = touchX - previousTouchX;
+    cube.rotation.y += deltaX * 0.01; // تحريك المكعب باللمس
+    previousTouchX = touchX;
+  }
+}, false);
+
+// تفاعل مع نهاية اللمس
+renderer.domElement.addEventListener('touchend', function (event) {
+  isDragging = false;
+}, false);
 
 // دورة التحديث للرسم
 function animate() {
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
   renderer.render(scene, camera);
 }
 animate();
